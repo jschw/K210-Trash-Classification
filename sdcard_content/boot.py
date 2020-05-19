ada7ce7cb141b7194cd57d2795d03b6b172a2f7e
Julian Schweigert <j_schweigert@gmx.de>
2020-05-19 21:32:49 +0200

    Added SD card content to repo

---
+++ sdcard_content/boot.py
@@ -0,0 +1,6 @@
+# Set operation mode for classification script
+# 0 -> continuous/test , 1 -> single shot , 2 -> multi shot
+op_mode = 0
+
+# Execute main script
+exec(open('/sd/models/trash/trash_main_program.py').read())
